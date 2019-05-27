c16892dd8b37c3efb8b80f2feecb6303fca058ca
jennl97 <jennhott97@gmail.com>
2019-05-26 22:45:30 -0500

    First commit of JS II

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,7 +1,6 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
-
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
-
+// Create a higher order function and invoke the callback function to test your work. 
+//You have been provided an example of a problem and a solution to see how this works with our items array. 
+//Study both the problem and the solution to figure out the rest of the problems.
 /* 
 
   //Given this problem: 
@@ -23,29 +22,57 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   });
 
 */
-
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
+getLength(items, (length) =>{
+  console.log(length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+    cb(arr.slice(-1));
 }
+last(items, (lastOne) =>{
+  console.log(lastOne);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 }
+sumNums(2, 5, (add) =>{
+  console.log(add);
+})
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 }
+
+multiplyNums(5, 6, (mult) =>{
+  console.log(mult);
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+   if (list.includes(item)){
+    let action = true;
+    return action;
+  } else {
+    let action= false;
+    return action;
+  };
+  cb(action);
 }
 
+contains("sugar", items, (cb)=> {
+  console.log(cb);
+})
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
